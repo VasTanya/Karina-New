4fc70aebf242cec8536b4f81a15ae83df3afd96f
Narek Kurghiyan <narek_kurghinyan@hotmail.com>
2023-08-31 11:20:26 +0400

    31.08 2

---
+++ client/src/components/AboutUs.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function AboutUs() {
+    return (
+        <div>
+            <div className="about_text_and_foto">
+                <img src="./img/about_foto.jpg" />
+                <div className="bg"></div>
+                <p>
+                    Located in the heart of Glendale, Karina’s Cake House has been
+                    serving
+                    exquisite cakes and pastries since 1999. The Armenian cake house
+                    is
+                    notorious for the impeccable quality and amazing flavor infused
+                    in all
+                    of its creations. Praised for its savory creations, Karina’s
+                    Cake
+                    House continues to strive for quality and delicious desserts
+                    that will
+                    always leave you craving more!
+                </p>
+            </div>
+        </div>
+
+    )
+}
+
+export default AboutUs
