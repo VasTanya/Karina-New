ef3480b534b243fa808cec6cda0fb7308c1e5b21
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2023-10-29 12:15:51 +0400

    ccc

--- api/Config/AllowedOrigins.js
+++ api/Config/AllowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "http://localhost:4000",
   "https://191.101.233.82",
   "https://karinas515.com",
+  "https://www.karinas515.com",
   "https://admin.karinas515.com",
   "https://api.karinas515.com",
 
