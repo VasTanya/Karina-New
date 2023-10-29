bf448f86314b25ff7384376d5fe4a17f0895f018
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2023-10-29 12:57:33 +0400

    hhh

--- api/Config/Credentials.js
+++ api/Config/Credentials.js
@@ -3,7 +3,17 @@ import allowedOrigins from "./AllowedOrigins.js";
 const credentials = (req, res, next) => {
   const origin = req?.headers?.origin;
   if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", "https://karinas515.com");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS, CONNECT, TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method"
+    );
     res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", 7200);
   }
   next();
 };
