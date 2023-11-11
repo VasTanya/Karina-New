964ea2455c871c65616f99fe15b8b83adda2f6de
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2023-11-11 13:51:12 +0400

    maaaaaaaiiiiilllll

--- client/src/redux/slice/getUrl_Request.js
+++ client/src/redux/slice/getUrl_Request.js
@@ -2,67 +2,70 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetch_Request = createAsyncThunk(
-    'fetch_request',
-    async (params, thunkAPI) => {
-        const { url, datainp } = params
-        const { data } = await axios({
-            method: 'post',
-            url: url,
-            data:  datainp
-        })
+  "fetch_request",
+  async (params, thunkAPI) => {
+    const { url, datainp } = params;
+    try {
+      const { data } = await axios.post(url, datainp, {
+        headers: {
+          "Content-Type": "application/json", // Adjust the content type based on your needs
+          // Add any other headers if required
+        },
+      });
 
-        console.log('url',url);
-        console.log('data',datainp);
-        console.log('dataaa',data);
-        
-        if(thunkAPI.length === 0 || typeof data === 'string' ){
-            return thunkAPI.rejectWithValue('error')
-        }
-        return thunkAPI.fulfillWithValue(data)
+      console.log("url", url);
+      console.log("data", datainp);
+      console.log("dataaa", data);
 
+      if (thunkAPI.length === 0 || typeof data === "string") {
+        return thunkAPI.rejectWithValue("error");
+      }
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      // Handle error here, e.g., logging or rejecting with a specific value
+      console.error("Error in fetch_Request:", error);
+      return thunkAPI.rejectWithValue(error.message);
     }
-)
+  }
+);
 
 const initialState = {
-    data_request: [
-         {
-            name: "",
-            phone: "",
-            email: "",
-            size: "",
-            filling: "",
-            img:"",
-            cakeCode: "",
-          },
-    ],
-    status1: 'loading',
-}
+  data_request: [
+    {
+      name: "",
+      phone: "",
+      email: "",
+      size: "",
+      filling: "",
+      img: "",
+      cakeCode: "",
+    },
+  ],
+  status1: "loading",
+};
 
 const getUrl_Request = createSlice({
-    name: 'requestToMail',
-    initialState,
-    redusers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetch_Request.pending, (state) => {
-                state.status1 = 'loading'
-                state.data_request = []
-            })
+  name: "requestToMail",
+  initialState,
+  redusers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetch_Request.pending, (state) => {
+        state.status1 = "loading";
+        state.data_request = [];
+      })
 
-            .addCase(fetch_Request.fulfilled, (state, action) => {
-                state.status1 = 'successful'
-                 state.data_request = action.payload
-                 console.log(action.payload);
-             
-            })
-            .addCase(fetch_Request.rejected, (state) => {
-                state.status1 = 'error'
-                 state.data_request = []
-             
-            })
-
-    }
-})
+      .addCase(fetch_Request.fulfilled, (state, action) => {
+        state.status1 = "successful";
+        state.data_request = action.payload;
+        console.log(action.payload);
+      })
+      .addCase(fetch_Request.rejected, (state) => {
+        state.status1 = "error";
+        state.data_request = [];
+      });
+  },
+});
 
 // export const {} = getUrl.actions;
 export default getUrl_Request.reducer;
