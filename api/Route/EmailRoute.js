1ea4b3d43db9dbcd33963d2006719d92613a032f
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2023-11-17 16:54:43 +0400

    sss

--- api/Route/EmailRoute.js
+++ api/Route/EmailRoute.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import EmailController from "../Controller/EmailController.js";
-import upload from "../Middleware/Multer.js";
 
 const emailRouter = Router();
 const { sendRequest, sendRequestMyDesign } = EmailController;
 
 emailRouter.post("/", sendRequest);
 
-emailRouter.post("/mydesign", upload.array("img"), sendRequestMyDesign);
+emailRouter.post("/mydesign", sendRequestMyDesign);
 
 export default emailRouter;
