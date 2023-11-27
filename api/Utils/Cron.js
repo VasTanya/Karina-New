d5b5f53295823b97c25f30b532f074fb07120d11
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2023-11-27 18:34:51 +0400

    bbb

--- api/Utils/Cron.js
+++ api/Utils/Cron.js
@@ -10,7 +10,7 @@ const Cron = () => {
 
     console.log("Current directory:", currentDir);
 
-    logger.log(`Deleting files in directory: ${directoryPath}`);
+    logger.log(`Deleting all files in directory: ${directoryPath}`);
 
     fs.readdir(directoryPath, (err, files) => {
       if (err) {
@@ -18,31 +18,14 @@ const Cron = () => {
         return;
       }
 
-      const currentTime = Date.now();
-      const oneWeekInMillis = 7 * 24 * 60 * 60 * 1000; // One week in milliseconds
-
       files.forEach((file) => {
-        console.log("====================================");
-        console.log("FOREACH FILE: ", file);
-        console.log("====================================");
         const filePath = path.join(directoryPath, file);
 
-        fs.stat(filePath, (error, stats) => {
-          if (error) {
-            logger.error(`Error getting file stats ${filePath}: ${error}`);
-            return;
-          }
-
-          const fileAgeInMillis = currentTime - stats.ctime.getTime();
-
-          if (fileAgeInMillis >= oneWeekInMillis) {
-            fs.unlink(filePath, (deleteError) => {
-              if (deleteError) {
-                logger.error(`Error deleting file ${filePath}: ${deleteError}`);
-              } else {
-                logger.log(`Deleted file: ${filePath}`);
-              }
-            });
+        fs.unlink(filePath, (deleteError) => {
+          if (deleteError) {
+            logger.error(`Error deleting file ${filePath}: ${deleteError}`);
+          } else {
+            logger.log(`Deleted file: ${filePath}`);
           }
         });
       });
