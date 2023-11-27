673ca5daa9a2588a80d1e8c3282389cc184fe012
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2023-11-27 15:03:54 +0400

    bbb

--- api/Utils/Mailer.js
+++ api/Utils/Mailer.js
@@ -27,7 +27,7 @@ const mailer = async (type, data, template) => {
         type === "request"
           ? `New Request For ${data.cakeCode}`
           : `New design request from ${data.email}`,
-      html: template,
+      html: type === "request" ? "" : template,
     });
 
     return {
