673185a301ad6a8be15c140e25c112cc53d5ee61
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2023-11-27 12:35:18 +0400

    bbb

--- api/Route/EmailRoute.js
+++ api/Route/EmailRoute.js
@@ -7,11 +7,12 @@ const { sendRequest, sendRequestMyDesign } = EmailController;
 
 emailRouter.post("/", sendRequest);
 
-emailRouter.post("/mydesign", (req, res) => {
+// Use the middleware before calling the route handler
+emailRouter.post("/mydesign", upload.array("img"), (req, res) => {
   console.log("====================================");
-  console.log("ROUTE: ", req.files);
+  console.log("ROUTE: ", req.files); // Use req.files for arrays
   console.log("====================================");
-  upload.array("img"), sendRequestMyDesign;
+  sendRequestMyDesign(req, res); // Call the route handler
 });
 
 export default emailRouter;
