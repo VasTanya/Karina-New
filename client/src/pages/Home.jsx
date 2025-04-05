1e665926937ce214b4148416de32ff305b820860
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2025-04-05 19:55:29 +0400

    Added github action v5 test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -13,9 +13,9 @@ function Home() {
 
   const getUrlFirstFoto = async () => {
     const queryString = `select=${encodeURIComponent(JSON.stringify(["md"]))}`;
-    const urlFirstFoto = `${process.env.REACT_APP_API_URL}/albums/firstPhoto?${queryString}`;
+    const urlFirstFoto = `${process.env.REACT_APP_API_URL || ""}/albums/firstPhoto?${queryString}`;
     console.log(
-      `${process.env.REACT_APP_API_URL}/albums/firstPhoto?${queryString}`
+      `${process.env.REACT_APP_API_URL || ""}/albums/firstPhoto?${queryString}`
     );
     dispatch(fetchFirstFoto({ urlFirstFoto }));
   };
