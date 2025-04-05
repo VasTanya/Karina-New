b02a46407d348d960c0268773f9925bd7463be77
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2025-04-05 21:12:04 +0400

    Final mods

--- client/src/firebase/config/firebase.js
+++ client/src/firebase/config/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-import { serviceAccount } from "./firebaseCredentials";
+import { serviceAccount } from "./firebase.config";
 
 let app, storage;
 try {
