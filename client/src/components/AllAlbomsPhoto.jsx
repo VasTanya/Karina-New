48504073b5469b7ae805d8e1c0d23ef9f5253280
Narek Kurghinyan <123357429+Narek0MadNation@users.noreply.github.com>
2025-04-05 19:17:17 +0400

    Added github action v4

--- client/src/components/AllAlbomsPhoto.jsx
+++ client/src/components/AllAlbomsPhoto.jsx
@@ -28,6 +28,7 @@ function AllAlbomsPhoto() {
   useEffect(() => {
     getUrl_Id_Albums();
     window.scrollTo(0, 0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentPage, id]);
 
   const onClickPages = (num) => {
